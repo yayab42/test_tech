0c8717c9bdcdc9ac9639667bd0aa73f827f67dd8
yayab <yannis.bounia@outlook.com>
2024-01-20 00:34:30 +0100

    Feat/ Added : models.py for both users and consult

--- test_tech/test_tech/consult_app/models.py
+++ test_tech/test_tech/consult_app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_patient = models.BooleanField(default=False)
+    is_doctor = models.BooleanField(default=False)
+
+
+class Patient(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    patient_id = models.CharField(max_length=20, unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    address = models.TextField()
+
+
+class Doctor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+
+class Consultation(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    date = models.DateField()
+    description = models.TextField()
+    type_choices = [
+        ('visite', 'Visite'),
+        ('suivi', 'Suivi'),
+        ('operation', 'Opération'),
+    ]
+    consultation_type = models.CharField(max_length=10, choices=type_choices)
